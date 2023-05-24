a00548c914095daebcb028bec8430baa0c215909
Ian Ridley <ian.ridley@procept.com.au>
2023-05-24 10:58:14 +1000

    feature: add architecture files for ARM64 & AARCH64

---
+++ make/arch/arm64.mk
@@ -0,0 +1,8 @@
+#
+# ARCH/ARM64.MK -- Definitions for ARM64 CPU and architectures.
+#
+# Copyright (c) 2023 Procept Pty. Ltd. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-Procept
+#
+# Note: ARM64 is also known as AARCH64 on some platforms.
+#
